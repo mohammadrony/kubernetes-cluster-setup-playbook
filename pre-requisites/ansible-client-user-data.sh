a466301f801562b1d8ff48e750f51ca156b0df30
Md. Asaduzzaman Rony <ronyku1829@gmail.com>
2024-02-12 11:00:38 +0600

    upd

--- pre-requisites/ansible-client-user-data.sh
+++ pre-requisites/ansible-client-user-data.sh
@@ -3,7 +3,8 @@ useradd ansible
 passwd -d ansible
 mkdir -p /home/ansible/.ssh
 chmod 700 /home/ansible/.ssh
-echo "<public-key>" >> /home/ansible/.ssh/authorized_keys
+echo "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABgQCosTmuzcpipFHNgClXxhVx5Vzo52tng89G9MA2qrfiwAmTu5YXDdM1nbJG0jjQKrtFjdmWVAo2C/IzbYlB6I56GdxjNympPh2cHBNPhTxFBx+T10sMf6Q2Nh2YRZosW9/TQopDBxI8ilsE+zPIqg2ulCdo2s6lhgVaz42MTEm7JRDJrHCeOTLc3gOoWD2vFjdhcS4n1R5sLUmfpV40Ls/U0DUfvKZpFep6gek3Qmtt/W2hfAMjK1nww+6OXdBVOfQVu2Fnd54cdzXiNd7jDhN5QSCsuLApFfwRBuXS92gLeQnT1FbHeAe/EJDgU9RcIph1n+MnsaXln0LbZgHLqEulcRvlPEuTnrdTs/eKMBs5WDOaRqoa6ngWvB6WO2wh9/VhJwrvXC9zkN8Kba5doulqhweoi0jEPeBj9akuBHTJvLXJU3G3S9ykVPZ4oIn8UBD6wsJDK6zgSZyVxeSQhNBPfyVmPtQOER6BOgo0jwScTVSdupNvnAGgVqsKUZHjAdU= ansible@dsi-Inspiron-15-5510
+" >> /home/ansible/.ssh/authorized_keys
 chown ansible:ansible -R /home/ansible/.ssh
 echo 'ansible ALL=(ALL) NOPASSWD: ALL' > /etc/sudoers.d/ansible
 chmod 600 /home/ansible/.ssh/authorized_keys
