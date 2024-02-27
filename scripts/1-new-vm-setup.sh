dfe9ba062f68ec4aafd608d7c54cb0eea84a6d7f
Md. Asaduzzaman Rony <ronyku1829@gmail.com>
2024-02-28 00:11:01 +0600

    Update hosts for k8s node.

--- scripts/1-new-vm-setup.sh
+++ scripts/1-new-vm-setup.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-# Initial VM setup
+# Time and hostname setup
 sudo timedatectl set-timezone Asia/Dhaka
 sudo echo "k8s-node" > /etc/hostname
+
+# Update k8s hosts
+# Replace (nnn.nnn.nnn.nnn) with ip address
+sudo echo "nnn.nnn.nnn.nnn k8s-master" >> /etc/hosts
+sudo echo "nnn.nnn.nnn.nnn k8s-worker-1" >> /etc/hosts
+sudo echo "nnn.nnn.nnn.nnn k8s-worker-N" >> /etc/hosts
 
 # Setup new user
 sudo useradd username
